04fe6691207c2f2b081edc2c32719480e719187b
stn6mazia <nicolas.mazia.666@gmail.com>
2017-10-08 07:06:53 -0300

    modelo exemplar

---
+++ perfis/migrations/0001_initial.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Perfil',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('nome', models.CharField(max_length=255)),
+                ('email', models.CharField(max_length=255)),
+                ('telefone', models.CharField(max_length=15)),
+                ('nome_empresa', models.CharField(max_length=255)),
+            ],
+        ),
+    ]
