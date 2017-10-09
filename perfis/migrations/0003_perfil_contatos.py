6c4904414db429e79255bd9d586380beb2ef917b
stn6mazia <nicolas.mazia.666@gmail.com>
2017-10-08 22:42:59 -0300

    commit

---
+++ perfis/migrations/0003_perfil_contatos.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('perfis', '0002_convite'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='perfil',
+            name='contatos',
+            field=models.ManyToManyField(related_name='_perfil_contatos_+', to='perfis.Perfil'),
+        ),
+    ]
